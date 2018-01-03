3544c05181c837fe2d52ce2a927c248a9a6d0102
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2018-01-03 02:35:50 +0200

    Code refactoring.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,9 +14,40 @@ import com.sorting.algorithms.RecursiveInsertionSort.RecursiveInsertionSort;
 import com.sorting.algorithms.SelectionSort.SelectionSort;
 import com.sorting.algorithms.ShellSort.ShellSort;
 import com.sorting.algorithms.Sort;
-import com.sorting.tester.Tester;
+import com.sorting.tester.Config;
+import com.sorting.tester.SortTimer;
+import com.sorting.tester.tester.MultipleTester;
+import com.sorting.tester.tester.SingleTester;
+import com.sorting.tester.tester.Tester;
+import com.sorting.tester.timer.MillisTimer;
+import com.sorting.tester.timer.NanoTimer;
+import com.sorting.tester.timer.SecondTimer;
+import com.sorting.tester.timer.Timer;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+
+    private enum TimeUnits {
+        NANOS, MILLIS, SECONDS
+    }
+
+    private static final TimeUnits TIME_UNIT = TimeUnits.SECONDS;
+    private static final int MIN_ARRAY_LENGTH = 10;
+    private static final int MAX_ARRAY_LENGTH = 1000;
+    private static final int STEP = 1;
+    private static final int ITERATIONS = 100;
+    private static final String PATH_PREFIX = "results/";
+    private static final String PATH_SUFFIX = ".sort";
+    private static final double TIME_RANGE = 0.3;
+    private static final boolean MULTIPLE = true;
+    private static final int THREADS_NUMBER = 2;
+
+    private static Config config;
+    private static Timer timer;
+    private static ExecutorService executor;
 
     public static void main(String[] args) {
         test(new BubbleSort());
@@ -33,10 +64,96 @@ public class Main {
         test(new RecursiveInsertionSort());
         test(new SelectionSort());
         test(new ShellSort());
-        test(new RadixSort());
+        //test(new RadixSort());
+        executorShutdown();
     }
 
     private static void test(final Sort sort) {
-        new Tester(sort).run();
+        final Tester tester = createTester(sort);
+        tester.test();
+    }
+
+    private static Tester createTester(final Sort sort) {
+        final String name = getSortName(sort);
+        final SortTimer timer = createSortTimer(sort);
+        final Config config = getConfig();
+        final Tester tester = new SingleTester(name, timer, config);
+        return checkMultiple(tester);
+    }
+
+    private static Tester checkMultiple(final Tester tester) {
+        final Tester resultTester;
+        if (MULTIPLE) {
+            final Executor executor = getExecutor();
+            resultTester = new MultipleTester(tester, executor);
+        } else {
+            resultTester = tester;
+        }
+        return resultTester;
+    }
+
+    private static ExecutorService getExecutor() {
+        if (executor == null) {
+            executor = createExecutor();
+        }
+        return executor;
+    }
+
+    private static ExecutorService createExecutor() {
+        return Executors.newFixedThreadPool(THREADS_NUMBER);
+    }
+
+    private static void executorShutdown() {
+        if (executor != null) {
+            executor.shutdown();
+            executor = null;
+        }
+    }
+
+    private static SortTimer createSortTimer(final Sort sort) {
+        final Timer timer = getTimer();
+        return new SortTimer(sort, timer);
+    }
+
+    private static Timer getTimer() {
+        if (timer == null) {
+            timer = createTimer();
+        }
+        return timer;
+    }
+
+    private static Timer createTimer() {
+        final Timer timer;
+        if (TIME_UNIT == TimeUnits.NANOS) {
+            timer = new NanoTimer();
+        } else if (TIME_UNIT == TimeUnits.SECONDS) {
+            timer = new SecondTimer();
+        } else {
+            timer = new MillisTimer();
+        }
+        return timer;
+    }
+
+    private static Config getConfig() {
+        if (config == null) {
+            config = createConfig();
+        }
+        return config;
+    }
+
+    private static Config createConfig() {
+        return new Config(
+                MIN_ARRAY_LENGTH,
+                MAX_ARRAY_LENGTH,
+                STEP,
+                ITERATIONS,
+                TIME_RANGE,
+                PATH_PREFIX,
+                PATH_SUFFIX
+        );
+    }
+
+    private static String getSortName(final Sort sort) {
+        return sort.getClass().getSimpleName();
     }
 }
--- src/main/java/com/sorting/tester/SortTimer.java
+++ src/main/java/com/sorting/tester/SortTimer.java
@@ -1,29 +1,22 @@
 package com.sorting.tester;
 
 import com.sorting.algorithms.Sort;
+import com.sorting.tester.timer.Timer;
 
 public final class SortTimer {
 
-    private static final boolean NANO = true;
-
     private final Sort sort;
+    private final Timer timer;
 
-    private final int[] array;
-
-    public SortTimer(final Sort sort, final int[] array) {
+    public SortTimer(final Sort sort, final Timer timer) {
         this.sort = sort;
-        this.array = new int[array.length];
-        System.arraycopy(array, 0, this.array, 0, array.length);
+        this.timer = timer;
     }
 
-    public long sort() {
-        final long start = time();
-        this.sort.sort(this.array);
-        final long end = time();
-        return (end - start);
-    }
-
-    private long time() {
-        return NANO ? System.nanoTime() : System.currentTimeMillis();
+    public double calcTime(final int[] array) {
+        this.timer.start();
+        this.sort.sort(array);
+        this.timer.stop();
+        return this.timer.time();
     }
 }
